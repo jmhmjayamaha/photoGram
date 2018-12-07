4a6ce9f5384732b84d06587f350c5c69a79c7cfc
jmhmjayamaha <jmhmjayamaha@gmail.com>
2018-12-07 12:09:55 +0530

    user drop downn list

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,15 +11,37 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
 
   isLoggedIn: boolean = false;
+  name: string;
+  email: string;
+  uid: string;
 
   constructor(private _userService : UserService,
               private router: Router
     ) { }
 
   ngOnInit() {
+
+    this._userService.statusChanged.subscribe(userData => {
+      if(userData) {
+        this.name = userData.fullname;
+        this.email = userData.email;
+        this.uid = userData.uid;
+      } else {
+        this.name = null;
+        this.email = null;
+        this.uid = null;
+      }
+    })
+
     firebase.auth().onAuthStateChanged(userData => {
       if(userData && userData.emailVerified) {
         this.isLoggedIn = true;
+        const user = this._userService.getUser();
+
+        this.name = user.fullname;
+        this.email = user.email;
+        this.uid = user.uid;
+
       }
       else {
         this.isLoggedIn = false;
--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -1,17 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  statusChanged: any = new EventEmitter<any>();
   constructor() { }
 
   set(userFromDb) {
     localStorage.setItem('user', JSON.stringify(userFromDb));
+    this.statusChanged.emit(userFromDb);
   }
 
+  getUser() {
+    const user =  localStorage.getItem('user');
+    return JSON.parse(user);
+  }
   destroy() {
     localStorage.removeItem('user');
+    this.statusChanged.emit(null);
   }
 }
