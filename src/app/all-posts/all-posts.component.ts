15e9442e2c3efa14e10b9ce5f93d60e5361f379a
jmhmjayamaha <jmhmjayamaha@gmail.com>
2018-12-12 17:01:40 +0530

    updating all-post component

--- src/app/all-posts/all-posts.component.ts
+++ src/app/all-posts/all-posts.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import * as firebase from "firebase";
-import _ from "lodash";
+import _ from 'lodash';
 
 @Component({
   selector: 'app-all-posts',
@@ -9,13 +9,14 @@ import _ from "lodash";
 })
 export class AllPostsComponent implements OnInit, OnDestroy {
 
+  loadMoreRef: any;
   allRef: any;
   all: any = [];
 
   constructor() { }
 
   ngOnInit() {
-    this.allRef = firebase.database().ref("allPosts").limitToLast(3);
+    this.allRef = firebase.database().ref("allPosts").limitToFirst(3);
     this.allRef.on("child_added" , data => {
       this.all.push({
         key: data.key,
@@ -25,15 +26,34 @@ export class AllPostsComponent implements OnInit, OnDestroy {
     })
   }
 
-  // onLoadMore(){
-  //   if(this.all.length > 0) {
-  //     const lastLoadedPost = _.last(this.all);
+  onLoadMore(){
+    if (this.all.length > 0) {
+      const lastLoadedPost = _.last(this.all);
+      const lastLoadedPostKey = lastLoadedPost.key;
 
-  //   }
-  // }
+      this.loadMoreRef = firebase.database().ref('allPosts').startAt(null, lastLoadedPostKey).limitToFirst(3 + 1);
+
+      this.loadMoreRef.on('child_added', data => {
+
+        if (data.key === lastLoadedPostKey) {
+          return;
+        } else {
+          this.all.push({
+            key: data.key,
+            data: data.val()
+          });
+        }
+
+      });
+
+    }
+  }
 
   ngOnDestroy() {
     this.allRef.off();
+    if(this.loadMoreRef) {
+      this.loadMoreRef.off();
+    }
   }
 
 }
