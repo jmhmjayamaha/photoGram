bbdc7cf9b3747bd7e15369333347a0b3ac50362a
jmhmjayamaha <jmhmjayamaha@gmail.com>
2018-12-14 14:07:37 +0530

    following and favorite updating

--- src/app/favarites/favarites.component.ts
+++ src/app/favarites/favarites.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-favarites',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavaritesComponent implements OnInit {
 
+  favoriteList: any = [];
+  
   constructor() { }
 
   ngOnInit() {
+    const uid = firebase.auth().currentUser.uid;
+    const favRef = firebase.database().ref('favorite').child(uid);
+
+    favRef.once('value').then(snapshot => {
+      const favoriteObj = snapshot.val();
+      this.favoriteList = _.values(favoriteObj);
+    });
   }
 
 }
--- src/app/following/following.component.ts
+++ src/app/following/following.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DatabaseService } from '../shared/database.service';
+import _ from 'lodash';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-following',
   templateUrl: './following.component.html',
   styleUrls: ['./following.component.css']
 })
-export class FollowingComponent implements OnInit {
+export class FollowingComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  refArray: any = [];
+  postList: any = [];
+
+  constructor(private _database : DatabaseService) { }
 
   ngOnInit() {
+    const uid = firebase.auth().currentUser.uid;
+    const followRef = firebase.database().ref('follow').child(uid);
+
+    followRef.once('value', data => {
+      const uidListOfOtherUsers = _.keys(data.val());
+      this.getPostsFromOtherUsers(uidListOfOtherUsers);
+    });
+  }
+
+  getPostsFromOtherUsers(uidList) {
+    for (let count = 0; count < uidList.length; count++) {
+      this.refArray[count] = this._database.getUserPostsList(uidList[count]);
+      this.refArray[count].on('child_added', data => {
+        this.postList.push({
+          key: data.key,
+          data: data.val()
+        });
+      });
+    }
+  }
+
+  ngOnDestroy() {
+    _.forEach(this.refArray, ref => {
+      if (ref && typeof(ref) === 'object') {
+        ref.off();
+      }
+    });
   }
 
 }
